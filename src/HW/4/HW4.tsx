45060160ed818dceb3b1b95337a83c5bc306f6e5
GodDennis <denpodreckiy@mail.ru>
2023-10-21 13:05:03 +0300

    bag odd even styles

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import s from './HW4.module.css';
-import { Button } from './Button';
-import { Input } from './Input';
+import { useState } from "react";
+import s from "./HW4.module.css";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import S from "./HW4.module.css";
 
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
@@ -12,40 +13,42 @@ export const HW4 = () => {
   // - дописать функциональность (где указано комментариями)
   // - приложение должно компилироваться и запускаться в браузере
 
-  const [currentText, setCurrentText] = useState('');
+  const [currentText, setCurrentText] = useState("");
   const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
+    "То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)",
   ]);
 
   const handleSave = () => {
     setTexts([currentText, ...texts]);
-    setCurrentText('');
+    setCurrentText("");
   };
 
   return (
-    <div id={'hw04'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
-      {/*)}*/}
+    <div id={"hw04"}>
+      {currentText ? (
+        <h1 id={"hw04-text"}>{currentText}</h1>
+      ) : (
+        <h1 id={"hw04-default-text"}>Здесь появится новое дело</h1>
+      )}
 
-      {/*<Input НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Input setCurrentText={setCurrentText} currentText={currentText} />
 
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Button callBack={handleSave} name="Save" />
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: "50px" }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={"hw04-tasks"}>
+        {texts.map((el, index) => {
+          return (
+            <li
+              key={index}
+              id={`hw04-task-${index}`}
+              className={!(index % 2) ? S.chetNechet : ""}>
+              {el}
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 };
-
